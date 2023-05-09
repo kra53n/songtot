f1c475054be5fe726b451b56b0a7c22c3af7c940
Krai53n <greasha46@gmail.com>
2023-05-09 10:37:37 +0300

    del excess var

--- songtot/app/handlers/common_commands.py
+++ songtot/app/handlers/common_commands.py
@@ -2,9 +2,10 @@ from aiogram import Dispatcher, types
 
 
 async def greeting(message: types.Message):
-    to_print = 'Добро пожаловать в бота, который скачивает музыку из Ютуба.' + \
-               '\n\nВведите /search, чтобы начать поиск.'
-    await message.answer(to_print)
+    await message.answer(
+        'Добро пожаловать в бота, который скачивает музыку из Ютуба.'
+        '\n\nВведите /search, чтобы начать поиск.'
+    )
 
 
 def register_start_option(dp: Dispatcher):
